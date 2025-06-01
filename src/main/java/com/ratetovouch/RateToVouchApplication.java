4c5cec0afeb083fbaf18c18909f357ba5ff363a1
PavelKPP <kppworks@gmail.com>
2025-06-01 14:10:38 -0400

    Initial commit

---
+++ src/main/java/com/ratetovouch/RateToVouchApplication.java
@@ -0,0 +1,13 @@
+package com.ratetovouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateToVouchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateToVouchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ratetovouch/RateToVouchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratetovouch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateToVouchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
